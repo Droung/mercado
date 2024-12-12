dbbb686c987308f781af550a9edb88a3a8a53181
Droung <drounguwu@gmail.com>
2024-12-11 23:49:23 -0600

    Vendedor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,34 +5,29 @@ import Carousel from './components/Carousel';
 export default function Home() {
   return (
     <>
-        <div className="image-container" >
-          
-          <ImageBox src="/img/one.jpg" alt="No found"/>
-          <ImageBox src="/img/luffy.jpg"  alt="No found"/>
-          <ImageBox src="/img/da.jpg" alt="No found"/>
-          <ImageBox src="/img/naruto.jpg" alt="No found"/>
-          <ImageBox src="/img/bl.jpg" alt="No found"/>
-          <ImageBox src="/img/pica.jpg" alt="No found"/>
+      <div className="image-container">
+        <ImageBox src="/img/one.jpg" alt="Not found"/>
+        <ImageBox src="/img/luffy.jpg" alt="Not found"/>
+        <ImageBox src="/img/da.jpg" alt="Not found"/>
+        <ImageBox src="/img/naruto.jpg" alt="Not found"/>
+        <ImageBox src="/img/bl.jpg" alt="Not found"/>
+        <ImageBox src="/img/pica.jpg" alt="Not found"/>
+      </div>
 
-        </div>
+      <div className="image-container">
+        <ImageBox src="/img/jjk.jpg" alt="Not found"/>
+        <ImageBox src="/img/goku.jpg" alt="Not found"/>
+        <ImageBox src="/img/mha.jpg" alt="Not found"/>
+        <ImageBox src="/img/dr.jpg" alt="Not found"/>
+        <ImageBox src="/img/jojos.jpg" alt="Not found"/>
+        <ImageBox src="/img/sl.jpg" alt="Not found"/>
+      </div>
 
-        <div className="image-container" >
-          
-          <ImageBox src="/img/jjk.jpg" alt="No found"/>
-          <ImageBox src="/img/goku.jpg"  alt="No found"/>
-          <ImageBox  src="/img/mha.jpg" alt="No found"/>
-          <ImageBox src="/img/dr.jpg" alt="No found"/>
-          <ImageBox src="/img/jojos.jpg" alt="No found"/>
-          <ImageBox src="/img/sl.jpg" alt="No found"/>
+      <h1 className='title'>Relacionados</h1>
 
-        </div>
-
-        <h1 className='title'>Relacionados</h1>
-
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
         <Carousel />
-        </div>
-              
+      </div>
     </>
   );
 }
